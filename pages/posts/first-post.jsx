be9e2c33b3aa687ad848675c89056839124ee264
Mehul Mohan <mehulmpt@gmail.com>
2020-08-09 05:52:02 +0530

    Lab 3 solutions

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,9 +1,10 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import styles from './styles.module.scss'
 
 export default function () {
 	return (
-		<div>
+		<div id="codedamn-post-container" className={styles.post}>
 			<h1>First Post</h1>
 			<Link href="/">
 				<a>Back to Home</a>
